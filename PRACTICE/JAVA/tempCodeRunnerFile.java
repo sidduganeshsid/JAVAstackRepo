964479756910ea4bf7a5a09ff62b8fcd28b8a52b
sidduganeshsid <sidduganeshengineer@gmail.com>
2024-03-21 02:02:26 +0530

    initial

---
+++ PRACTICE/JAVA/tempCodeRunnerFile.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// class Main{
+//     public static void main(String args[])
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         float base,height, area;
+//         System.out.println("Enter the Base and Height :");
+
+//         base = sc.nextFloat();
+//         height = sc.nextFloat();
+
+//         area = 1f/2f * base * height;
+
+//         System.out.println("Area of triangle using h and b : "+area);
+
+//     }
+// }
